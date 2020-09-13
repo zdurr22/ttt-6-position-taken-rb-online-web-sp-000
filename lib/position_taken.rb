04574c575badc9a80fba79854fb5266b0c9c420f
Zachary Durr <zdurr22@gmail.com>
2020-09-13 00:00:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 # code your #position_taken? method here!
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+board = [" "," "," "," "," "," "," "," "," "]
 
 def position_taken?(board, index)
 
